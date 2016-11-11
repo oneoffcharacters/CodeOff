9a5c76cd66f0171e0637a2f89ff8a07c53ab2f72
Sherman <shc410@gmail.com>
2016-11-11 12:21:14 -0800

    reconfig webpack

---
+++ client/components/test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Test extends React.Component {
+	render() {
+		return(
+			<h1>HELLO WORLD</h1>
+		)
+	}
+}
--- client/file.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<p>HELLO</p>, document.getElementById('app'));
