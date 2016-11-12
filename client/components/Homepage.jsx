4199c4709267b9ff1f2afb67c4b4c0148af384f0
Sherman <shc410@gmail.com>
2016-11-12 15:44:34 -0800

    fix error in homepage.jsx

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import { expect } from 'chai';
-import { mount, shallow, render } from 'enzyme';
+import Landing from './Landing';
+import Header from './Header'
 
-import Home from '../client/components/Homepage';
-
-describe('<Home />', () => {
-
-  it('should render a <Header /> component', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.find('Header')).to.have.length(1);
-  })
-
-  it('should render a <Landing /> component', () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper.find('Landing')).to.have.length(1);
-  })
-})  
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Landing />
+      </div>
+    )
+  }
+}
