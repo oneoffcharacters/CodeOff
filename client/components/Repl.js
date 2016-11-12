e463623c996b67c1c527f29a76323cb992971c95
Guy Thomas <guythomas721@gmail.com>
2016-11-12 15:49:50 -0800

    Modify routes to show REPL

--- client/components/Repl.js
+++ client/components/Repl.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/javascript");
+
 class Repl extends React.Component {
+    constructor(props) {
+	    super(props);
+	    this.state = {
+	  	};
+    }
+
+  componentDidUpdate() {
+  	console.log('this.createREPL', this.createREPL)
+  	this.createREPL();
+  }
+
+  createREPL() {
+  	var editor = ace.edit("editor");
+  	editor.setTheme("ace/theme/monokai");
+  	editor.getSession().setMode("ace/mode/javascript");
+  }
+
   render() {
     return (
-      <div>
+      <div id="editor">
       	REPL
       </div>
     )
