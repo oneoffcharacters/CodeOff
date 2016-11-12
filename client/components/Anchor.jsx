8372d9472fdff14d271b0513cdcc2c2c07418360
Guy Thomas <guythomas721@gmail.com>
2016-11-12 15:02:23 -0800

    Resolve conflicts in Anchor

--- client/components/Anchor.jsx
+++ client/components/Anchor.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './test';
-import Home from './Homepage';
+import Home from './HomePage';
+import { Route, Router, hashHistory } from './react-router'
+import Repl from './Repl'
+import Dashboard from './Dashboard'
 
 export default class Anchor extends React.Component {
 	render() {
 		return(
-			<div>
+			<Router history={hashHistory}>
 				<h1 className='title'>HELLO WEBPACK</h1>
-				<Test />
-        <Home />
-			</div>
+				<Route path="/" component={Home}/>
+				<Route path="/repl" component={Repl}/>
+				<Route path="/dashboard" component={About}/>
+			</Router>
 		)
 	}
 }
