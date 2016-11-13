69140692fb007743b7754bfdf2911bb7b4287e14
Guy Thomas <guythomas721@gmail.com>
2016-11-12 16:42:19 -0800

    BROKEN ROUTING - Add Repl component and routing

--- client/components/Anchor.jsx
+++ client/components/Anchor.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './test';
-import Home from './HomePage';
-import { Route, Router, hashHistory } from 'react-router'
+import App from './App';
+import { Route, Router, hashHistory, IndexRedirect } from 'react-router'
 import Repl from './Repl'
 import Dashboard from './Dashboard'
+import Landing from	'./Landing'
 
 export default class Anchor extends React.Component {
 	render() {
 		return(
 			<Router history={hashHistory}>
-				<Route path="/" component={Home}>
+				<Route path="/" component={App}>
+					<IndexRedirect to="/landing" />
+					<Route path="/landing" component={Landing}/>
 					<Route path="/repl" component={Repl}/>
 					<Route path="/dashboard" component={Dashboard}/>
 				</Route>
