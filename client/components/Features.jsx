74d3cb9bff8c28afb2db2b3d7244f8cd7b971750
Jesse Mavi <jesse.mavi@gmail.com>
2016-11-14 15:08:07 -0800

    Adds CSS styling to homepage

--- client/components/Features.jsx
+++ client/components/Features.jsx
@@ -3,22 +3,25 @@ import React from 'react';
 export default class Features extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Get Pro, And Prove It To Others</h2>
+      <div className="Features">
+        <h2 className="features-heading">Get Pro, And Prove It To Others</h2>
+
         <div>
-          <img src={''} />
-          <h4>COMPETE</h4>
-          <p>Compete against friends, random people from across the globe or yourself.</p>
+          <img src="" />
+          <h4 className="feature-title">COMPETE</h4>
+          <p className="feature-text">Compete against friends, random people from across the globe or yourself.</p>
         </div>
+
         <div>
-          <img src={''} />
-          <h4>IMPROVE</h4>
-          <p>Challenge yourself with real problems and discuss them with your competition afterward to always improve.</p>
+          <img src="" />
+          <h4 className="feature-title">IMPROVE</h4>
+          <p className="feature-text">Challenge yourself with real problems and discuss them with your competition afterward to always improve.</p>
         </div>
+
         <div>
-          <img src={''} />
-          <h4>SHOWCASE</h4>
-          <p>Showcase your victories to future employers. They will be able to see your ranking and your victories.</p>
+          <img src="" />
+          <h4 className="feature-title">SHOWCASE</h4>
+          <p className="feature-text">Showcase your victories to future employers. They will be able to see your ranking and your victories.</p>
         </div>
       </div>
     )
