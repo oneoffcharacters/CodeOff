47aba199b44a1f120e5fed1d0e11fa739fe42598
rdgifford <robbie.gifford@gmail.com>
2016-11-15 11:49:40 -0800

    (feat) Move sh script into seperate deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# if pull request to branch production
+if [ $TRAVIS_PULL_REQUEST_BRANCH == 'production' ] ; then
+  echo 'Attempting deploy...'
+else
+  echo 'Not deploying, pull request branch is not production.'
+fi
