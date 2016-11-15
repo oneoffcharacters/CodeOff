89e3841d0911dc49eac641055bd36582136eeff5
rdgifford <robbie.gifford@gmail.com>
2016-11-15 14:54:18 -0800

    (feat) Testing travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # if pull request to branch production
+echo $TRAVIS_PULL_REQUEST_BRANCH && $TRAVIS_BRANCH
 if [ $TRAVIS_PULL_REQUEST_BRANCH == 'production' ] ; then
   echo 'Attempting deploy...'
 else
