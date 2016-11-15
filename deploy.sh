a38087e279b07031f8840a2d54d0dfd8ce720dfe
rdgifford <robbie.gifford@gmail.com>
2016-11-15 13:55:24 -0800

    (feat) Adding docker to travis build process

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # if pull request to branch production
 if [ $TRAVIS_PULL_REQUEST_BRANCH == 'production' ] ; then
   echo 'Attempting deploy...'
-  docker --help
 else
   echo 'Not deploying, pull request branch is not production.'
 fi
