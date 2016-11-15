d8b2ac15f5c24da905428a2118a112bc2bd93e89
rdgifford <robbie.gifford@gmail.com>
2016-11-15 12:22:55 -0800

    (feat) Testing docker use inside deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # if pull request to branch production
 if [ $TRAVIS_PULL_REQUEST_BRANCH == 'production' ] ; then
   echo 'Attempting deploy...'
+  docker --help
 else
   echo 'Not deploying, pull request branch is not production.'
 fi
