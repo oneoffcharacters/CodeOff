6452e492092f81af2569d9d8e878496151eb1880
Sherman <shc410@gmail.com>
2016-11-15 18:39:52 -0800

    working in subheader.spec file

--- test/Subheader.spec.js
+++ test/Subheader.spec.js
@@ -11,11 +11,22 @@ describe('<Subheader />', () => {
     expect(wrapper.find('button')).to.have.length(3);
   })
 
+  it('should be able to accept click handlers as props', () => {
+    const tester = sinon.spy();
+    const wrapper = mount(<Subheader sendCode={tester} />)
+    expect(wrapper.prop('sendCode')).to.eql(tester);
+  })
+
   it('should fire event handler when submit button is clicked', () => {
     const tester = sinon.spy();
     const wrapper = mount(<Subheader sendCode={tester} />);
     wrapper.find('button.sendCodeBtn').simulate('click');
     expect(tester.calledOnce).to.equal(true);
+  })
+
+  it('should contain stuff', () => {
+    const wrapper = shallow(<Subheader />);
+    expect(wrapper.contains('div')).to.be.true;
   })
 
 })
