9c59ad5a87c95b96c9a8e1d43d9ed6649b8bebdf
rdgifford <robbie.gifford@gmail.com>
2016-11-15 19:55:40 -0800

    (feat) Add pushing to DockerHub to travis

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# if pull request to branch production
+# if pull request to branch production !!! and not from fork
 if [ $TRAVIS_BRANCH == 'production' ] ; then
   echo 'Attempting deploy...'
   docker build -q -t rdgifford/thesis .
   # docker run -p 3000:3000 rdgifford/thesis
   docker login -u $DOCKER_LOGIN_USER -p $DOCKER_LOGIN_PASSWORD
-  
+  docker push rdgifford/thesis
   docker ps -a
+  # ssh root@104.131.4.226
 else
   echo 'Not deploying, pull request branch is not production.'
 fi
