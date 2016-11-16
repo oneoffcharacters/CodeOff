05fa245cb2b9e1d704fa94fa1fd9b7f79e5312a8
rdgifford <robbie.gifford@gmail.com>
2016-11-15 18:40:20 -0800

    (feat) Add to continuous deploy, encrypt pass and user

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ if [ $TRAVIS_BRANCH == 'production' ] ; then
   echo 'Attempting deploy...'
   docker build -q -t rdgifford/thesis .
   # docker run -p 3000:3000 rdgifford/thesis
-  docker login
+  docker login -u $DOCKER_LOGIN_USER -p $DOCKER_LOGIN_PASSWORD
   
   docker ps -a
 else
