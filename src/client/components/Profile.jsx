d589f1e6ffab0b663c2b4e6aae2053288bae4f6f
Jesse Mavi <jesse.mavi@gmail.com>
2016-11-15 16:15:06 -0800

    Adds leaderboard and profile headers

--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -7,7 +7,15 @@ export default class Profile extends React.Component {
         <h1>Profile</h1>
 
         <div className="ProfileContent">
-          
+          <div className="ProfileContentHeader">
+            <h4>Wins</h4>
+            <h4>Draws</h4>
+            <h4>Losses</h4>
+          </div>
+
+          <div>
+            
+          </div>
         </div>
       </div>
     )
