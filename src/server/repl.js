e6eca0e547c4014381ff4b6fd09f3548965ad625
Guy Thomas <guythomas721@gmail.com>
2016-11-15 21:01:16 -0800

    Clean up and comment repl.js

--- src/server/repl.js
+++ src/server/repl.js
@@ -1,13 +1,10 @@
-// Running Code in REPL
 const repl = require('repl');
 const stream = require('stream');
 
-
 module.exports = {
   runCode: (code, path, callback) => {
-    console.log('The code is ', code);
-    
-    //sanitize the repl input
+
+    //Clear the white space from every line
     var codeSanitized = code.split('\n').map((section) => {
       var trimmed = section.trim();
       var firstChar = trimmed.charAt(0);
@@ -17,44 +14,27 @@ module.exports = {
       return section;
     }).join('\n');
 
-    console.log('code sant', codeSanitized);
-
-    /**
-      * @name input
-      * @desc Push to-be-evaluated code to readable stream, ready by REPL.
-      */
+    //Convert the input string into a readable stream for use of the REPL module
     var input = new stream.Readable();
     input._read = function noop() {
       input.push(codeSanitized);
       input.push(null);
     };
 
-    /**
-      * @name output
-      * @desc Writable stream written by REPL while evaluating input code.
-      */
+    //Prepare a writeable stream for the REPL to write the result to
     var output = new stream.Writable();
     var data = '';
-    output._write = function noop(chunk, encoding, callback) { // why is it called NOOP?
-        data += chunk;
-        callback();
+    output._write = function noop(chunk, encoding, callback) {
+      data += chunk;
+      callback();
     };
 
-
-    /**
-      * @name repl.start
-      * @desc Sends a Readable stream to the repl server and outputs a writeable stream after eval
-      * @param {input, output} readable and writable streams
-      * @returns {nothing}
-      */
-      var server = repl.start({input: input, output:output, terminal: false, ignoreUndefined: true});
-      server.on('exit', () => {
-        console.log('Received "exit" event from repl!');
-        console.log('data after exit', data)
-        data = data.replace(/(\.)+/g, '');
-        data = data.slice(2)
-        callback(data);
-      });
-
+    //Evaluate the code and run the callback on the result
+    var server = repl.start({input: input, output:output, terminal: false, ignoreUndefined: true});
+    server.on('exit', () => {
+      data = data.replace(/(\.)+/g, '');
+      data = data.slice(2)
+      callback(data);
+    });
   }
 };
--- src/server/server.js
+++ src/server/server.js
@@ -3,24 +3,17 @@ const morgan = require('morgan');
 const app = express();
 const runCode = require('./repl').runCode
 const bodyParser = require('body-parser');
-const Chance = require('chance')
-const chance = new Chance();
 
-
-
-
-// const util = require('util')
-// REQUEST LOGGING
+//Middleware
 app.use(morgan('dev'));
-
-// STATIC ASSETS
 app.use(express.static('src/client'));
-// app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//Create the server and the socket connection
 const server = app.listen(3000, () => console.log('App listening on port 3000'))
 const io = require('socket.io')(server);
 
+//Run the code from the editor and return the result
 app.post('/api/repl', (req, res) => {
 	runCode(req.body.code, req.path, (data) => {
 		if (!res.headerSent) {
