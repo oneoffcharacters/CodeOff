59c89fd76fceb65dfa39785ee572afb2c196dbae
Jesse Mavi <jesse.mavi@gmail.com>
2016-11-16 16:15:00 -0800

    fix conflicts in Leaderboard, Profile.jsx

--- src/client/components/Leaderboard.jsx
+++ src/client/components/Leaderboard.jsx
@@ -4,8 +4,6 @@ export default class Leaderboard extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< ef475d1fbd5b90080f1e1f5cff57ca3ead3bfd9a
-
         <h2>Leaderboard</h2>
 
         <div className="LeaderboardContent">
@@ -21,26 +19,6 @@ export default class Leaderboard extends React.Component {
 
         </div>
 
-||||||| merged common ancestors
-        <h1>Leaderboard</h1>
-=======
-
-        <h2>Leaderboard</h2>
-
-        <div className="LeaderboardContent">
-          <div className="LeaderboardContentHeader">
-            <h4>Rank</h4>
-            <h4>User</h4>
-            <h4>Score</h4>
-          </div>
-
-          <div className="LeaderboardContentMain">
-            
-          </div>
-
-        </div>
-
->>>>>>> Adds styling for dashboard page
       </div>
     )
   }
--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -4,8 +4,6 @@ export default class Profile extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< 052cb3b4a573063f76cfc5c99d9fa9194c926e0a
-<<<<<<< ef475d1fbd5b90080f1e1f5cff57ca3ead3bfd9a
         <h2>Profile</h2>
 
         <div className="ProfileContent">
@@ -23,32 +21,6 @@ export default class Profile extends React.Component {
             
           </div>
         </div>
-||||||| merged common ancestors
-        <h1>Profile</h1>
-=======
-        <h1>Profile</h1>
-||||||| merged common ancestors
-        <h1>Profile</h1>
-=======
-        <h2>Profile</h2>
->>>>>>> Adds styling to Dashboard
-
-        <div className="ProfileContent">
-          <div className="ProfileContentHeader">
-            <h4>Wins</h4>
-            <h4>Draws</h4>
-            <h4>Losses</h4>
-          </div>
-
-          <div>
-            <h4 className="ProfileContentTitle">Previous Games</h4>
-          </div>
-
-          <div className="ProfileContentMain">
-            
-          </div>
-        </div>
->>>>>>> Adds styling for dashboard page
       </div>
     )
   }
