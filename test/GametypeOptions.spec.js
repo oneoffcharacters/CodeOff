fde5f45ad67fc2442595b478ce57028ebb2883ef
Guy Thomas <guythomas721@gmail.com>
2016-11-18 09:19:04 -0800

    Fix test cases to account for renamed functions

--- test/GametypeOptions.spec.js
+++ test/GametypeOptions.spec.js
@@ -12,21 +12,21 @@ describe('<GametypeOptions />', () => {
     expect(swrapper.find('li')).to.have.length(4);
   });
 
-  it('should have props for startGame and endGame', function () {
-    expect(swrapper.props().startGame).to.be.defined;
-    expect(swrapper.props().endGame).to.be.defined;
+  it('should have props for startFreshGame and terminateGame', function () {
+    expect(swrapper.props().startFreshGame).to.be.defined;
+    expect(swrapper.props().terminateGame).to.be.defined;
   });
 
   it('should start a solo game when the solo li is clicked', () => {
     const tester = sinon.spy();
-    const wrapper = mount(<GametypeOptions startGame={tester} endGame={tester} />);
+    const wrapper = mount(<GametypeOptions startFreshGame={tester} terminateGame={tester} />);
     wrapper.find('li').at(0).simulate('click');
     expect(tester.calledOnce).to.equal(true);
   });
 
   it('should start a battle game when the battle li is clicked', () => {
     const tester = sinon.spy();
-    const wrapper = mount(<GametypeOptions startGame={tester} endGame={tester} />);
+    const wrapper = mount(<GametypeOptions startFreshGame={tester} terminateGame={tester} />);
     wrapper.find('li').at(1).simulate('click');
     expect(tester.calledOnce).to.equal(true);
   });
