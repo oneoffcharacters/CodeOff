e55a8703fab94f58c4cf2271dd80ea77a37743d2
rdgifford <robbie.gifford@gmail.com>
2016-11-18 15:07:55 -0800

    Add programmatic mocha testing with child process and writing to files

--- test/TestingService.spec.js
+++ test/TestingService.spec.js
@@ -1,13 +1,13 @@
-// const vm = require('vm');
-// const chai = require('chai');
-// const expect = chai.expect;
-// const should = chai.should;
-// const mocha = require('mocha');
-// const describe = mocha.describe;
-// const it = mocha.it;
+const vm = require('vm');
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should;
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
 
-// const code = 'describe("test", () => { it("should be true", () => { expect(true).to.be.true }) })';
+const code = 'describe("test", () => { it("should be true", () => { expect(true).to.be.true }) })';
 
-// const script = new vm.Script(code);
+const script = new vm.Script(code);
 
-// console.log(script.runInNewContext({ describe, it, expect, should }))
+console.log(script.runInNewContext({ describe, it, expect, should }))
