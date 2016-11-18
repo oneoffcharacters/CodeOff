4589a72bdca0d81a0e300adfa26f88ee218f0592
Jesse Mavi <jesse.mavi@gmail.com>
2016-11-18 15:06:43 -0800

    Fixed merge conflicts

--- githubAuth.js
+++ githubAuth.js
@@ -6,6 +6,32 @@ var socialAuth = require('./socialAuth.config');
 var authInit = require('./socialAuthInit');
 
 passport.use(new GitHubStrategy({
-  
-}));
+    clientID: socialAuth.github.clientID,
+    clientSecret: socialAuth.github.clientSecret,
+    callbackURL: socialAuth.github.callbackURL
+  },
+
+  function(accessToken, refreshToken, profile, done) {
+    var searchQuery = {
+      github_displayName: profile.displayName
+    };
+    var updates = {
+      github_username: profile.username
+    };
+    // update the user if they exist or add a new user
+    User.findOneAndUpdate(searchQuery, updates, function(err, user) {
+      if(err) {
+        return done(err);
+      } else {
+        return done(null, user);
+      }
+    });
+  }
+
+));
+
+// serialize user into session
+authInit();
+
+module.exports = passport;
 
