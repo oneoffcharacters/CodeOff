2ce3fa379b4ebaa48d1a1db10cc521d0d810b04c
rdgifford <robbie.gifford@gmail.com>
2016-11-17 19:56:28 -0800

    (feat) Add skeleton of testing service, read / write to file with mocha

--- src/test_service/server.js
+++ src/test_service/server.js
@@ -2,10 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser')
+const exec = require('child_process').exec
+const port = 3000;
+const mock = require('mock-fs');
+const code = 'describe("test", () => {expect(true).to.be(true)})';
+const fs = require('fs')
+
 
 // REQUEST LOGGING
 app.use(morgan('dev'));
 
-app.post('/api/test', (req, res) => {
+var testCode = (challenge, callback) => {
+  exec(`mocha --reporter json ${challenge}`, (error, stdout, stderr) => {
+    if(error) {
+      console.error(error)
+    } else {
+      callback(stderr, stdout)
+    }
+  })
+}
 
+app.get('/', (req, res) => {
+  testCode((err, data) => {
+    res.status(200)
+    res.send(data);
+  })
+})
+
+app.listen(3000, () => {
+  console.log(`Testing service running on port ${port}`);
 })
---
+++ test/TestingService.spec.js
@@ -0,0 +1,13 @@
+// const vm = require('vm');
+// const chai = require('chai');
+// const expect = chai.expect;
+// const should = chai.should;
+// const mocha = require('mocha');
+// const describe = mocha.describe;
+// const it = mocha.it;
+
+// const code = 'describe("test", () => { it("should be true", () => { expect(true).to.be.true }) })';
+
+// const script = new vm.Script(code);
+
+// console.log(script.runInNewContext({ describe, it, expect, should }))
