3d2ea6793c57ffed91db4973578aedd1d125ed81
rdgifford <robbie.gifford@gmail.com>
2016-11-18 15:07:55 -0800

    Add programmatic mocha testing with child process and writing to files

--- src/test_service/server.js
+++ src/test_service/server.js
@@ -4,16 +4,21 @@ const app = express();
 const bodyParser = require('body-parser')
 const exec = require('child_process').exec
 const port = 3000;
-const mock = require('mock-fs');
-const code = 'describe("test", () => {expect(true).to.be(true)})';
+const path = require('path');
+// const mock = require('mock-fs');
+// const code = 'describe("test", () => {expect(true).to.be(true)})';
 const fs = require('fs')
-
+Promise.promisifyAll(fs);
 
 // REQUEST LOGGING
 app.use(morgan('dev'));
 
-var testCode = (challenge, callback) => {
-  exec(`mocha --reporter json ${challenge}`, (error, stdout, stderr) => {
+// BODY PARSING
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+var testCode = (challengePath, callback) => {
+  exec(`mocha --reporter json ${challengePath}`, (error, stdout, stderr) => {
     if(error) {
       console.error(error)
     } else {
@@ -22,13 +27,29 @@ var testCode = (challenge, callback) => {
   })
 }
 
-app.get('/', (req, res) => {
-  testCode((err, data) => {
+/*
+ * req.body
+ *    .challengeName should be a string
+ *    .user should be a string
+ */
+
+app.get('/api/test', (req, res) => {
+  var attemptPath = path.join(__dirname, 'user_attempts', req.body.user, `${req.body.challengeName}.js`);
+  // write user code to file
+  fs.writeFileAsync(attemptPath, req.body.userCode)
+    .then((data) => {
+      var solutionPath = path.join
+      // write result code to file
+      fs.writeFileAsync();
+    });
+      // write exports to testfile
+  var challengePath = path.join(__dirname, 'challenge_tests', `${req.body.challengeName}.spec.js`);
+  testCode(challengePath, (err, data) => {
     res.status(200)
     res.send(data);
   })
 })
 
 app.listen(3000, () => {
-  console.log(`Testing service running on port ${port}`);
+  console.log(`Testing Service running on port ${port}`);
 })
