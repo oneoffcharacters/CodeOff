f1942e01d288f6d98627a5f46a208458680f8fa2
Sherman <shc410@gmail.com>
2016-11-17 19:03:33 -0800

    create a temp QuestionPrompt, consider making into a modal instead

--- src/client/components/QuestionPrompt.jsx
+++ src/client/components/QuestionPrompt.jsx
@@ -4,24 +4,26 @@ export default class QuestionPrompt extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			prompt: '',
-			ex1: 'this is an example ex1',
-			ex2: 'this is an example ex2'
+			prompt: 'before mount',
+			ex1: '',
+			ex2: ''
 		}
 	}
 
-	ComponentWillMount() {
+	componentWillMount() {
 		console.log('dummy fetch database request!');
 		this.setState({
-			prompt: 'reset the prompts initial state'
+			prompt: 'reset the prompts initial state',
+      ex1: 'grab from db',
+      ex2: 'grab from db'
 		});
 	}
 
 	render() {
 		return (
 			<div className='QPContainer'>
-				<h4 className='QPMessage'>{this.state.prompt}</h4>
-				<h4 className='QPSample'>Sample Test Cases</h4>
+				<h3 className='QPMessage'>{this.state.prompt}</h3>
+				<h5 className='QPSample'>Sample Test Cases</h5>
 				<p className='QPex1'>{this.state.ex1}</p>
 				<p className='QPex2'>{this.state.ex2}</p>
 			</div>	
