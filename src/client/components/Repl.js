fb1ca3b00dfc14e54cc4501d251cea58c7cf037e
Guy Thomas <guythomas721@gmail.com>
2016-11-18 15:54:47 -0800

    Add question modal

--- src/client/components/Repl.js
+++ src/client/components/Repl.js
@@ -276,6 +276,7 @@ class Repl extends React.Component {
             </div>
             <div className="repl-panel col-sm-12 col-md-6 no-pad">
               <div id="console-terminal-editor" className="home-console"></div>
+              <QuestionPrompt />
             </div>
           </div> 
         </div>
