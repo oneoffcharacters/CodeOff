0b088df52a8ddaca4ce4930b2b7b403896300769
Sherman <shc410@gmail.com>
2016-11-17 20:33:02 -0800

    create controller and models for posting and serving challenges

--- src/db/controllers/challengeCtrl.js
+++ src/db/controllers/challengeCtrl.js
@@ -1,3 +1,50 @@
+const mongoose = require('mongoose');
+const Challenge = require('../models/Challenge');
+
 exports.serveChallenge = (req, res) => {
-  res.send('testing controller endpoint');
-}
+  Challenge.findOne({name: req.body.name}).exec((err, challenge) => {
+    if (err) {
+      console.log(err);
+    }
+
+    if (!challenge) {
+      res.status(404).send('challenge not found');
+    } else {
+      res.status(200).send(challenge);
+    }
+
+  });
+};
+
+// for when posting challenges is available
+// exports.postChallenge = (req, res) => {
+//   Challenge.findOne({name: req.body.name}).exec((err, challenge) => {
+//     if (err) {
+//       console.log(err);
+//     }
+
+//     if (challenge) {
+//       res.status(409).send('Challenge name already taken!');
+//     } else {
+
+//       const newChallenege = new Challenge({
+//         name: req.body.name,
+//         difficulty: req.body.difficulty,
+//         solutions: req.body.solutions,
+//         prompt: req.body.prompt,
+//         ex1: req.body.ex1,
+//         ex2: req.body.ex2
+//       });
+
+//       newChallenege.save((err, challenge) => {
+//         if (err) {
+//           console.log(err);
+//           res.status(500).send('error saving to db! ', err);
+//         } else {
+//           res.status(200).send(challenge);
+//         }
+//       });
+
+//     }
+//   });
+// };
--- src/db/models/Challenge.js
+++ src/db/models/Challenge.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const challengeSchema = new Schema({
-  name: String,
+  name: {type: String, unique: true, required: true},
   difficulty: String,
   solutions: String,
   prompt: String,
   ex1: String,
   ex2: String,
-  name: {type: String, unique: true, required: true},
   // User has many Challenges, Challenge belongs to User
-  creator: ObjectId,
+  // creator: ObjectId,
 })
 
 // db is not yet defined, create and import connection before uncommenting
-const Challenge = db.model('Challenge', challengeSchema);
+const Challenge = mongoose.model('Challenge', challengeSchema);
 
 exports = Challenge;
