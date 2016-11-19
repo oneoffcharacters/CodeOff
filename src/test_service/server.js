cc4768bc4312ef6db885fd682dc360e3214be84d
rdgifford <robbie.gifford@gmail.com>
2016-11-18 23:28:31 -0800

    Add to testing service with tmp file storage and retrieval for mocha testing w/ exec on mocha shell command

--- src/test_service/server.js
+++ src/test_service/server.js
@@ -1,24 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
+// Packages
 const bodyParser = require('body-parser')
 const exec = require('child_process').exec
-const port = 3000;
+const Promise = require('bluebird');
+const express = require('express');
+const morgan = require('morgan');
 const path = require('path');
-// const mock = require('mock-fs');
-// const code = 'describe("test", () => {expect(true).to.be(true)})';
-const fs = require('fs')
+const tmp = require('tmp');
+const fs = require('fs');
+// Env variables
+const app = express();
+const port = 3001;
+// const test = 'const expect = require("chai").expect; describe("test", () => { it("should be true", () => { expect(true).to.be.true }) })';
+const test = 'require("chai").expect; describe("test", () => { it("should be true", () => { expect(attempt).to.be(solution) }) })' 
+
 Promise.promisifyAll(fs);
 
-// REQUEST LOGGING
-app.use(morgan('dev'));
+// Install mocha and chai in temp directory
+var findTemp = tmp.fileSync({prefix: 'find', keep: true});
+var tempPath = path.parse(findTemp.name).dir
+findTemp.removeCallback();
+exec(`cd ${tempPath} && npm install mocha chai`, (error, stdout, stderr) => {
+  if(error) {
+    console.error(error)
+  } else {
+    console.log(stderr, stdout)
+  }
+})
 
-// BODY PARSING
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json());
-
-var testCode = (challengePath, callback) => {
-  exec(`mocha --reporter json ${challengePath}`, (error, stdout, stderr) => {
+// Run mocha on given testpath
+const runMocha = (testPath, callback) => {
+  exec(`mocha --reporter json ${testPath}`, (error, stdout, stderr) => {
     if(error) {
       console.error(error)
     } else {
@@ -27,29 +38,64 @@ var testCode = (challengePath, callback) => {
   })
 }
 
-/*
- * req.body
- *    .challengeName should be a string
- *    .user should be a string
- */
-
 app.get('/api/test', (req, res) => {
-  var attemptPath = path.join(__dirname, 'user_attempts', req.body.user, `${req.body.challengeName}.js`);
-  // write user code to file
-  fs.writeFileAsync(attemptPath, req.body.userCode)
-    .then((data) => {
-      var solutionPath = path.join
-      // write result code to file
-      fs.writeFileAsync();
-    });
-      // write exports to testfile
-  var challengePath = path.join(__dirname, 'challenge_tests', `${req.body.challengeName}.spec.js`);
-  testCode(challengePath, (err, data) => {
     res.status(200)
-    res.send(data);
-  })
 })
 
-app.listen(3000, () => {
-  console.log(`Testing Service running on port ${port}`);
+app.post('/api/test', (req, res) => {
+  console.log('INSIDE OF POST');
+  console.log(test)
+  console.log(req)
+
+  // Assign code to variables
+  var attempt = req.body.attempt;
+  var solution = req.body.solution;
+  // test = req.body.test;
+
+  // Create temp files
+  var attemptFile = tmp.fileSync({prefix: 'attempt-', postfix: '.js', keep: true});
+  var solutionFile = tmp.fileSync({prefix: 'solution-', postfix: '.js', keep: true});
+  var testFile = tmp.fileSync({prefix: 'test-', postfix: '.spec.js', keep: true});
+
+  // Derive file bases from parsed temp files
+  var solutionFileBase = path.parse(solutionFile.name).base;
+  var attemptFileBase = path.parse(attemptFile.name).base;
+
+  // Import with file bases
+  console.log('TESTING', test);
+  test = (`var solution = require('./${solutionFileBase}');`).concat(test);
+  test = (`var attempt = require('./${attemptFileBase}');`).concat(test);
+	console.log(test);
+
+  // Write temp files
+  fs.writeFileSync(attemptFile.name, attempt)
+  fs.writeFileSync(solutionFile.name, solution)
+  fs.writeFileSync(testFile.name, test)
+
+  runMocha(testFile.name, (err, data) => {
+    // Send err and data from mocha
+    res.status(200).json({
+      err: err,
+      data: data,
+    });
+
+    // Remove temp files
+    testFile.removeCallback();
+    solutionFile.removeCallback();
+    attemptFile.removeCallback();
+  });
 })
+
+// REQUEST LOGGING
+app.use(morgan('dev'));
+
+// BODY PARSING
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json());
+
+// SERVER
+app.listen(port, () => {
+  console.log(`Testing Service listening on port ${port}`);
+})
+
+module.exports = app;
--- test/TestingService.spec.js
+++ test/TestingService.spec.js
@@ -1,13 +1,35 @@
-const vm = require('vm');
-const chai = require('chai');
-const expect = chai.expect;
-const should = chai.should;
-const mocha = require('mocha');
-const describe = mocha.describe;
-const it = mocha.it;
+// const { expect, should } = require('chai');
+// const request = require('supertest');
+// const app = require('../src/test_service/server.js');
+// const fs = require('fs');
 
-const code = 'describe("test", () => { it("should be true", () => { expect(true).to.be.true }) })';
-
-const script = new vm.Script(code);
-
-console.log(script.runInNewContext({ describe, it, expect, should }))
+// describe('Testing service server', () => {
+//   describe('GET /api/test', () => {
+//     it('should respond with a 200 status code', () => {
+//       request(app)
+//       .get('/api/test')
+//       .expect(200);
+//     })
+//   });
+//   describe('POST /api/test', () => {
+//     it('should create attempt file based on given user, challengeName, and attempt', () => {
+//       var attemptPath = '../src/test_service/challenge_attempts/Guy/CHALLENGE.js';
+//       var attemptsData = {
+//         user: 'Guy',
+//         challengeName: 'CHALLENGE',
+//         attemptCode: 'var attempt = function(){ return true };'
+//       }
+//       request(app)
+//       .post('/api/test')
+//       .expect(200)
+//       .end(function(res){
+//         console.log('HELLO FROM THE OTHERSIDE!');
+//         console.log(err, res)
+//       })
+//       // var attemptFiles = fs.readdirSync(attemptPath);
+//       // expect(attemptFiles).to.be.a(Array);
+//       // expect(attemptFiles[0]).to.be('CHALLENGE.js');
+//       // get rid of file after creating it
+//     });
+//   })
+// });
