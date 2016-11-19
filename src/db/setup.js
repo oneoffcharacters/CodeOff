1f486d568604418b8cb2b0606822c34208a90053
Sherman <shc410@gmail.com>
2016-11-19 12:03:24 -0800

    move mongoose connection outside of server

---
+++ src/db/setup.js
@@ -0,0 +1,9 @@
+mongoose.connect('mongodb://localhost/codeoff');
+
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('Mongoose connection is on ');
+});
+
+module.exports = db;
