54eafdcd9e6923ec65d57fd5b9b26d996440ab49
Guy Thomas <guythomas721@gmail.com>
2016-11-19 16:15:59 -0800

    Add rendering of error message and change 'question' terminology to 'challenge'

---
+++ src/client/components/ChallengeCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class ChallengeCard extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+		document.getElementById('toggleChallenge').addEventListener('click', function (e) {
+		  e.preventDefault();
+		  if (document.getElementById('challengeModal').classList.contains('expanded')) {
+		    document.getElementById('challengeModal').classList.remove('expanded')
+		    this.classList.remove('fa-chevron-down');
+		    this.classList.add('fa-chevron-up');
+		  } else {
+		    document.getElementById('challengeModal').classList.add('expanded')
+		    this.classList.add('fa-chevron-down');
+		    this.classList.remove('fa-chevron-up');
+		  }
+		});
+	}
+
+
+	
+
+	render() {
+		const examples = this.props.challenge.examples ? this.props.challenge.examples : []
+
+		return (
+			<div id="challengeModal" className="challenge expanded">
+				<div className="challenge-title"><h2>{this.props.challenge.title || 'Start a game to get a challenge'}</h2></div>
+				<div className="challenge-prompt">{this.props.challenge.prompt}</div>
+				<div className="challenge-examples">{examples.map((example, index) => {
+					return <div key={index} className="challenge-example">{'Example ' + (parseInt(index) +1) + ': ' + example}</div>
+				})}</div>
+				<a href="#" id="toggleChallenge" className="closebtn fa fa-chevron-down"></a>
+			</div>
+		)
+	}
+};
