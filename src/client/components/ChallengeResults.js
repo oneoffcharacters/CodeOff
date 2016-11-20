c9a95c036ffc74ef084c313e4b4a2ec448b4ec95
Guy Thomas <guythomas721@gmail.com>
2016-11-20 10:00:57 -0800

    The challengeResults modal will correctly display your win / loss status

--- src/client/components/ChallengeResults.js
+++ src/client/components/ChallengeResults.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ChallengeResults = ({ terminateGame, nextRoundTimer }) => {
+const ChallengeResults = ({ terminateGame, nextRoundTimer , challengeResults }) => {
 	return (
 		<div className="result-card">
-			<div className="result-title">You Won!</div>
+			<div className="result-title">You {challengeResults}!</div>
 			<div className="result-timeleft">The next game will begin in {nextRoundTimer} seconds</div>
 			<div className="result-ordivider">---or---</div>
 			<div onClick={terminateGame} className="btn btn-default">Stop Playing</div>
