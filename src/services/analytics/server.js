6e66293f90e8d9d5451ddd0f7d080f3dacdff8e8
rdgifford <robbie.gifford@gmail.com>
2016-11-21 20:23:55 -0800

    (feat) Remove template server code copied from testing server

---
+++ src/services/analytics/server.js
@@ -0,0 +1,38 @@
+// Packages
+const bodyParser = require('body-parser')
+// const exec = require('child_process').exec
+const Promise = require('bluebird');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+// const tmp = require('tmp');
+const fs = require('fs');
+// Env variables
+const app = express();
+const port = 3002;
+
+Promise.promisifyAll(fs);
+
+// ---------- MIDDLEWARE ----------
+// REQUEST LOGGING
+app.use(morgan('dev'));
+
+// BODY PARSING
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+// --------------------------------
+
+// benchmarking
+// time to completion
+// linting /readability
+// brevity (lines of code)
+// modularity (problem depends on using solution later in code, deduct points if
+// user reworks previous soluton
+// no universal code scoring / no leaderboard
+
+// SERVER
+app.listen(port, () => {
+  console.log(`Analytics Service listening on port ${port}`);
+})
+
+module.exports = app;
