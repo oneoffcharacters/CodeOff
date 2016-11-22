230237f06c77dde8bf1fa580d6d0f3974bfd45dd
Sherman <shc410@gmail.com>
2016-11-21 19:04:51 -0800

    add User model controller

--- src/client/components/Signin.jsx
+++ src/client/components/Signin.jsx
@@ -1,6 +1,8 @@
 import React from 'React';
 import Header from './Header';
 import Footer from './Footer';
+const authUrl = 'http://localhost:3000/api/auth/login';
+
 
 export default class Signin extends React.Component {
 	constructor() {
@@ -11,6 +13,7 @@ export default class Signin extends React.Component {
 		};
 		this.onUserChangeHandle = this.onUserChangeHandle.bind(this);
 		this.onPwChangeHandle = this.onPwChangeHandle.bind(this);		
+		this.onFormSubmit = this.onFormSubmit.bind(this)''
 		this.onButtonPress = this.onButtonPress.bind(this);
 	}
 
@@ -22,13 +25,20 @@ export default class Signin extends React.Component {
 		this.setState({pw: e.target.value});
 	}
 
-	onButtonPress(e) {
+	onFormSubmit(e) {
 		e.preventDefault();
 		console.log(`this is the user ${this.state.user}, and this is the password ${this.state.pw}`);
+		
+		fetch('')
+
 		this.setState({
 			user: '',
 			pw: ''
 		})
+	}
+
+	onButtonPress() {
+		this.onFormSubmit();
 	}	
 
 	render () {
@@ -39,9 +49,9 @@ export default class Signin extends React.Component {
 					<h3>Welcome Back!</h3>
 					<h3>{this.state.user}</h3>
 					<h3>{this.state.pw}</h3>
-					<form action="submit">
-						<input type="text" onChange={this.onUserChangeHandle} placeholder="user" />
-						<input type="text" onChange={this.onPwChangeHandle} placeholder="password"/>
+					<form action="submit" onSubmit={this.onFormSubmit}>
+						<input type="text" name="userinp" onChange={this.onUserChangeHandle} placeholder="user" />
+						<input type="text" name="pwinp" onChange={this.onPwChangeHandle} placeholder="password"/>
 						<button onClick={this.onButtonPress}>Sign In!</button>
 					</form>
 
