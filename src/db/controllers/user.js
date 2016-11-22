e0348c972a060347b4811bc1891a8ba1447f4d5c
Sherman <shc410@gmail.com>
2016-11-21 19:23:14 -0800

    resolve bug in user controller

--- src/db/controllers/user.js
+++ src/db/controllers/user.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
+// exports.findUser = (name) => {
+//   console.log('FIND NAME', name)
+//   return User.findOne({ 'name': name })
+//     .then(User => {
+//         console.log('WITHINPROMISE NAME', User.name);
+//         return User;
+//       })
+//     .catch(err => {
+//       console.error(err);
+//     })
+// }
+
 exports.findUser = (name) => {
-  console.log('FIND NAME', name)
-  return Challenge.findOne({ 'name': name })
-    .then(challenge => {
-        console.log('WITHINPROMISE NAME', challenge.name);
-        return challenge;
-      })
+  return User.findOne({"name": name})
+    .then(user => {
+      console.log('user ', user);
+      return user;
+    })
     .catch(err => {
       console.error(err);
     })
