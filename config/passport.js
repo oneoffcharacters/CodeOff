4d4f541e2556d87789daef9c675d94f60f0293f5
rdgifford <robbie.gifford@gmail.com>
2016-11-21 19:19:10 -0800

    (fix) Add config files w/o sensitive information, fix auth es6 with use strict, add sensitive file in config to .gitignore

---
+++ config/passport.js
@@ -0,0 +1,46 @@
+const passport = require('passport'),  
+      User = require('../src/db/models/User'),
+      config = require('./main'),
+      JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt,
+      LocalStrategy = require('passport-local');
+
+const localOptions = { usernameField: 'email' };
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {  
+  User.findOne({ email: email }, function(err, user) {
+    if(err) { return done(err); }
+    if(!user) { return done(null, false, { error: 'Your login details could not be verified. Please try again.' }); }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false, { error: "Your login details could not be verified. Please try again." }); }
+
+      return done(null, user);
+    });
+  });
+});
+
+const jwtOptions = {  
+  // Telling Passport to check authorization headers for JWT
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  // Telling Passport where to find the secret
+  secretOrKey: config.secret
+};
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  console.log(payload);  
+  User.findById(payload._id, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+passport.use(jwtLogin);  
+passport.use(localLogin);
+
