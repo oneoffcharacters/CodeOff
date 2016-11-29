e8c3177507f1d3ca1d84ba0fa2d4bcfb96b2939e
Jesse Mavi <jesse.mavi@gmail.com>
2016-11-28 16:46:36 -0800

    Adds post request to /api/challenge on form submission

--- src/client/components/AddChallenge.jsx
+++ src/client/components/AddChallenge.jsx
@@ -22,6 +22,8 @@ export default class AddChallenge extends React.Component {
     this.onTemplateFunctionChangeHandle = this.onTemplateFunctionChangeHandle.bind(this);
     this.onExamplesChangeHandle = this.onExamplesChangeHandle.bind(this);
     this.onTestChangeHandle = this.onTestChangeHandle.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onButtonPress = this.onButtonPress.bind(this);
   }
 
   onTitleChangeHandle(e) {
@@ -57,7 +59,54 @@ export default class AddChallenge extends React.Component {
   }
 
   onFormSubmit(e) {
+    // e.preventDefault();
+    console.log('form submitted');
 
+    let challengeInputs = JSON.stringify({
+      title: this.state.title,
+      functionName: this.state.functionName,
+      difficulty: this.state.difficulty,
+      solutions: this.state.solutions,
+      prompt: this.state.prompt,
+      templateFunction: this.state.templateFunction,
+      examples: this.state.examples,
+      test: this.state.test
+    });
+
+    fetch('http://localhost:3000/api/challenge', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: challengeInputs 
+    })
+    .then(() => {
+      console.log('post request successful');
+      this.setState({
+        title: '',
+        functionName: '',
+        difficulty: '',
+        solutions: '',
+        prompt: '',
+        templateFunction: '',
+        examples: '',
+        test: ''
+      });
+      console.log('state after request to /api/challenge =', this.state); 
+    })
+    .catch(err => {
+      console.error(err);
+      this.setState({
+        title: '',
+        functionName: '',
+        difficulty: '',
+        solutions: '',
+        prompt: '',
+        templateFunction: '',
+        examples: '',
+        test: ''
+      });
+    });
   }
 
   onButtonPress() {
@@ -69,15 +118,16 @@ export default class AddChallenge extends React.Component {
       <div>
         <div>
           <h3>Create a new challenge!</h3>
-          <form>
-            <input type="text" value={'hello'} name="" onChange={this.onTitleChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onFunctionNameChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onDifficultyChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onSolutionsChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onPromptChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onTemplateFunctionChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onExamplesChangeHandle} placeholder="" />
-            <input type="text" value={'hello'} name="" onChange={this.onTestChangeHandle} placeholder="" />
+          <h3>{this.state.title}</h3>
+          <form action="submit" onSubmit={this.onFormSubmit}>
+            <input type="text" value={this.state.title} name="" onChange={this.onTitleChangeHandle} placeholder="title" />
+            <input type="text" value={this.state.functionName} name="" onChange={this.onFunctionNameChangeHandle} placeholder="function name" />
+            <input type="text" value={this.state.difficulty} name="" onChange={this.onDifficultyChangeHandle} placeholder="difficulty" />
+            <input type="text" value={this.state.solutions} name="" onChange={this.onSolutionsChangeHandle} placeholder="solutions" />
+            <input type="text" value={this.state.prompt} name="" onChange={this.onPromptChangeHandle} placeholder="prompt" />
+            <input type="text" value={this.state.templateFunction} name="" onChange={this.onTemplateFunctionChangeHandle} placeholder="template function" />
+            <input type="text" value={this.state.examples} name="" onChange={this.onExamplesChangeHandle} placeholder="examples" />
+            <input type="text" value={this.state.test} name="" onChange={this.onTestChangeHandle} placeholder="test" />
             <button onClick={this.onButtonPress}>Add Challenge</button>
           </form>
         </div>
