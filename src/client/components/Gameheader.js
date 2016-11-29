98e1541fefaccc5449ab7ba285d0902e2c6464d2
Guy <guythomas721@gmail.com>
2016-11-28 22:59:09 -0800

    Add mock question to speed up build workflow

--- src/client/components/Gameheader.js
+++ src/client/components/Gameheader.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import Gamestatus from './Gamestatus'
 
 
 const Gameheader = (props) => {
-	let players = props.playerDetails;
-	let current = props.gameStats.current;
-	let previous = props.gameStats.previous;
 	console.log(props);
+
+	// challengeProgress={this.state.challengeProgress}
+	// challenge={this.state.challenge} 
+	// gameProgress={this.state.gameProgress} 
+	// currentGameStats={this.state.currentGameStats} 
+	// playerNames={this.state.playerNames} />
+
+
 
 	return (
 		<div className="gameheader">
 			<div className="player">
-				<div className="profile">{players.me}</div>
+				<div className="profile"></div>
 				<div className="username"></div>
 			</div>
 			<div className="center-dashboard">
 				<div className="logo"></div>
 				<div className="game-dashboard">
+					{props.challenge.map((game, i) => (
+						<Gamestatus 
+							challengeProgress={props.challengeProgress}
+							challenge={props.challenge} 
+							gameProgress={props.gameProgress} 
+							currentGameStats={props.currentGameStats} 
+							playerNames={props.playerNames} />
+					))}
 					<div className="game-status"></div>
-					<div className="game-status current">{players.me}<br />{players.opponent}</div>
+					<div className="game-status current"></div>
 					<div className="game-status unplayed"></div>
 				</div>
 			</div>
---
+++ src/client/components/Gamestatus.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Gamestatus = (props) => {
+	console.log('Props in gamestatus', props)
+	//Decide if it is the current game
+		//If it is, assign this variable to the split config
+		//Else, create it as the singular mode
+			//Decide on the styling based on who one it
+
+	return (
+		<div className="gamestatus">
+			Gamestatus
+		</div>
+
+	)
+}
+
+export default Gamestatus
