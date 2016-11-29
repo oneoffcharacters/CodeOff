f369dacc418ef911c784613b600fd8bc80a809fe
Sherman <shc410@gmail.com>
2016-11-28 17:10:17 -0800

    added back _main.js in config

---
+++ config/_main.js
@@ -0,0 +1,8 @@
+module.exports = {  
+  // Secret key for JWT signing and encryption
+  'secret': '',
+  // Database connection information
+  'database': '',
+  // Setting port for server
+  'port':
+}
