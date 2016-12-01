003718ad01cdeb26d7d7ae4db25404ce25e43730
Sherman <shc410@gmail.com>
2016-12-01 12:28:01 -0800

    cleanup profile.jsx

--- src/client/components/Profile.jsx
+++ src/client/components/Profile.jsx
@@ -1,3 +1,4 @@
+// import packages
 import React from 'react';
 
 export default class Profile extends React.Component {
@@ -5,25 +6,19 @@ export default class Profile extends React.Component {
     return (
       <div>
         <h2>Profile</h2>
-
         <div className="ProfileContent">
           <div className="ProfileContentHeader">
             <h4>Wins</h4>
             <h4>Draws</h4>
             <h4>Losses</h4>
           </div>
-
           <div>
             <h4 className="ProfileContentTitle">Previous Games</h4>
           </div>
-
-          <div className="ProfileContentMain">
-            
-          </div>
+          <div className="ProfileContentMain"></div>
         </div>
       </div>
     )
   }
 }
-
 // ProfileContentMain: pass in props of a users' games and iterate over them and return an <li> with each matchup
