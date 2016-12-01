3ed02df49d524e1b39127022baedb88635dab7cc
Sherman <shc410@gmail.com>
2016-12-01 10:23:00 -0800

    cleanup app jsx

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,16 +1,14 @@
+// import packages
 import React from 'react';
 import Header from './Header'
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
     	<div className="container-fluid no-pad">
     		<Header />
     		{this.props.children}
     	</div>
-
     )
   }
 }
-
-export default App
