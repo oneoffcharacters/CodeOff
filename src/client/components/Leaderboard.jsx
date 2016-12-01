b10e6549223c9be4f05caf29d69598ca9d3b2bb8
Sherman <shc410@gmail.com>
2016-12-01 11:03:01 -0800

    cleanup leaderboard jsx

--- src/client/components/Leaderboard.jsx
+++ src/client/components/Leaderboard.jsx
@@ -1,3 +1,4 @@
+// import packages
 import React from 'react';
 
 export default class Leaderboard extends React.Component {
@@ -5,23 +6,16 @@ export default class Leaderboard extends React.Component {
     return (
       <div>
         <h2>Leaderboard</h2>
-
         <div className="LeaderboardContent">
           <div className="LeaderboardContentHeader">
             <h4>Rank</h4>
             <h4>User</h4>
             <h4>Score</h4>
           </div>
-
-          <div className="LeaderboardContentMain">
-            
-          </div>
-
+          <div className="LeaderboardContentMain"></div>
         </div>
-
       </div>
     )
   }
 }
-
 // LeaderboardContentMain: pass in props of all users and iterate over them and return an <li> with rank, user and score
