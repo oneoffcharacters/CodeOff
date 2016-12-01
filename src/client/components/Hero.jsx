a6d0fec481d190ed6f3943557416365a0fba5976
Sherman <shc410@gmail.com>
2016-12-01 10:55:00 -0800

    cleanup hero jsx

--- src/client/components/Hero.jsx
+++ src/client/components/Hero.jsx
@@ -1,3 +1,4 @@
+// import packages
 import React from 'react';
 import { Link } from 'react-router';
 
@@ -5,11 +6,10 @@ export default class Hero extends React.Component {
   render() {
     return (
       <div className="Hero">
-        <h1 className="HeroText">Compete in code<br />competitions in real<br />time across the globe.</h1>
-
+        <h1 className="HeroText">Compete in Code<br />Competitions in real<br />time across the globe.</h1>
         <div className="HeroButtons">
           <Link to="/gameroom"><button className="PlayNow" type="submit">Play Now</button></Link>
-          <button className="LearnMore" type="submit">Learn More</button>
+          <Link to="/lobby"><button className="LearnMore" type="submit">View Lobby</button></Link>
         </div>
       </div>
     )
