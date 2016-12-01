ff040784d306ec3998f48f00521eee2644d5e601
Guy <guythomas721@gmail.com>
2016-12-01 15:12:35 -0800

    Add powerup only being visible on battle

--- src/client/components/Powerups.jsx
+++ src/client/components/Powerups.jsx
@@ -1,19 +1,19 @@
 // import packages
 import React from 'react';
 
-const Powerups = ({powerups, usePowerup}) => {
+const Powerups = ({powerups, usePowerup, currentGameType}) => {
   const powerupList = Object.keys(powerups)
-  
-  return (
-    <div className="dropdown dropdown-inline">
-      <button id="powerup" className="btn btn-secondary" type="button" data-toggle="dropdown"></button>
-      <ul className="dropdown-menu">
-      {powerupList.map((powerup, i) => (
-        <li key = {i} onClick={() => usePowerup(powerup)}>{powerup + ": " + powerups[powerup].quantity}</li>
-      ))}
-      </ul>
-    </div>
-  )
-}
+
+    return (
+      <div className="dropdown dropdown-inline" >
+        <button id="powerup" className="btn btn-secondary" type="button" data-toggle="dropdown" style={(currentGameType != 'Battle') ? {display:'none'} : {}}></button>
+        <ul className="dropdown-menu">
+        {powerupList.map((powerup, i) => (
+          <li key = {i} onClick={() => usePowerup(powerup)}>{powerup + ": " + powerups[powerup].quantity}</li>
+        ))}
+        </ul>
+      </div>
+    )
+  } 
 
 export default Powerups
