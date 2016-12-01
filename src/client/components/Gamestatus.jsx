488be45495332b70dd1351471920ceeb746f0e83
Guy <guythomas721@gmail.com>
2016-12-01 14:51:40 -0800

    Fix gameplay issues between switching game styles

--- src/client/components/Gamestatus.jsx
+++ src/client/components/Gamestatus.jsx
@@ -2,14 +2,30 @@
 import React from 'react';
 
 const Gamestatus = (props) => {
-	if (props.gameNumber === props.challengeProgress) {
+	if (props.currentGameType === 'No Game') {
 		return (
-			<div className="gamestatus current-game">
-				{props.currentGameStats.opponent} {props.currentGameStats.me > 1 ? 'Tests' : 'Test'} Passing
-				<br />
-				{props.currentGameStats.me} {props.currentGameStats.opponent > 1 ? 'Tests' : 'Test'} Passing
+			<div className="gamestatus inactive">
+				<span className="gamestatus-score">No Game</span>
 			</div>
 			)
+	} else if (props.gameNumber === props.challengeProgress) {
+		if (props.currentGameType === 'Solo') {
+			return (
+				<div className="gamestatus won">
+					<span className="gamestatus-score">
+						{props.currentGameStats.score}
+					</span>
+				</div>
+				)
+		} else {
+			return (
+				<div className="gamestatus current-game">
+					{props.currentGameStats.opponent} {props.currentGameStats.opponent > 1 ? 'Tests' : 'Test'} Passing
+					<br />
+					{props.currentGameStats.me} {props.currentGameStats.me > 1 ? 'Tests' : 'Test'} Passing
+				</div>
+				)
+		}
 	} else if (props.gameNumber > props.challengeProgress) {
 		return (
 			<div className="gamestatus inactive">
