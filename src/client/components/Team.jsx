a667cbf41071e9656b0782542200c8144e6c02f6
Sherman <shc410@gmail.com>
2016-12-01 19:06:28 -0800

    finalize team jsx

--- src/client/components/Team.jsx
+++ src/client/components/Team.jsx
@@ -21,22 +21,22 @@ export default class Team extends React.Component {
 					<div className="about-profile">
 						<div className="profile-name">Guy Thomas</div>
 						<img className="about-pic" src={this.state.gt}></img>
-						<div className="profile-blurb">ipsum lorem stuff yea jkhasdjkasdjkasd basdbasdasbd bhasdhbsadbad bhasdbasdbasd bhasdbasdb</div>
+						<div className="profile-blurb">When not pimping up his Linkedin profile, Guy takes to the skies with his locked, stocked and four smokin rotors Phantom 4. <br /><br/></div>
 					</div>
 					<div className="about-profile">
 						<div className="profile-name">Jesse Mavi</div>
 						<img className="about-pic" src={this.state.jm}></img>
-						<div className="profile-blurb">ipsum lorem stuff yea jkhasdjkasdjkasd basdbasdasbd bhasdhbsadbad bhasdbasdbasd bhasdbasdb</div>
+						<div className="profile-blurb">Originally from Chicago, when not developing or learning programming Jesse enjoys taking fitness classes, reading and playing golf.</div>
 					</div>
 					<div className="about-profile">
 						<div className="profile-name">Robbie Gifford</div>
 						<img className="about-pic" src={this.state.rg}></img>
-						<div className="profile-blurb">ipsum lorem stuff yea jkhasdjkasdjkasd basdbasdasbd bhasdhbsadbad bhasdbasdbasd bhasdbasdb</div>
+						<div className="profile-blurb">I code. I have interests outside of coding. <br /><br /><br /></div>
 					</div>
 					<div className="about-profile">
 						<div className="profile-name">Sherman Chen</div>
 						<img className="about-pic" src={this.state.sc}></img>
-						<div className="profile-blurb">When not developing Sherman can be found distracting others with his fascination for cat based iOS games</div>
+						<div className="profile-blurb">A native San Franciscan, when not developing Sherman is most likely playing tennis, watching netflix, or pestering his co-workers with his fascination of cat based iOS games</div>
 					</div>
 				</div>
 			</div>
